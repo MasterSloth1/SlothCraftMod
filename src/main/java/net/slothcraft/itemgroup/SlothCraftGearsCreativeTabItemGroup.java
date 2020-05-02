df99335dbdf6dfebe513cd5117b217d905a89de3
MasterSloth <ajashbrook45@gmail.com>
2020-05-03 01:55:57 +1200

    add textures for items, blocks and some armour

---
+++ src/main/java/net/slothcraft/itemgroup/SlothCraftGearsCreativeTabItemGroup.java
@@ -0,0 +1,35 @@
+
+package net.slothcraft.itemgroup;
+
+import net.slothcraft.item.AluminumGearItemItem;
+import net.slothcraft.SlothCraftElements;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+
+@SlothCraftElements.ModElement.Tag
+public class SlothCraftGearsCreativeTabItemGroup extends SlothCraftElements.ModElement {
+	public SlothCraftGearsCreativeTabItemGroup(SlothCraftElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		tab = new ItemGroup("tabslothcraftgearscreativetab") {
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public ItemStack createIcon() {
+				return new ItemStack(AluminumGearItemItem.block, (int) (1));
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasSearchBar() {
+				return true;
+			}
+		}.setBackgroundImageName("item_search.png");
+	}
+	public static ItemGroup tab;
+}
