2955316b7103b28536d2f11340770dd1cdab481a
ajash <ajash@SlothsDesktop.fritz.box>
2020-05-07 12:10:52 +1200

    testing new name and added a couple more recipes for the plates

--- src/main/java/net/slothcraft/procedures/PlatesMakerBlockUpdateTickProcedure.java
+++ src/main/java/net/slothcraft/procedures/PlatesMakerBlockUpdateTickProcedure.java
@@ -1,6 +1,8 @@
 package net.slothcraft.procedures;
 
 import net.slothcraft.item.PlateHammerItemItem;
+import net.slothcraft.item.ChromiumPlateItemItem;
+import net.slothcraft.item.AmethystPlateItemItem;
 import net.slothcraft.item.AluminumPlateItemItem;
 import net.slothcraft.SlothCraftElements;
 
@@ -97,6 +99,146 @@ public class PlatesMakerBlockUpdateTickProcedure extends SlothCraftElements.ModE
 				TileEntity inv = world.getTileEntity(new BlockPos((int) x, (int) y, (int) z));
 				if (inv != null && (inv instanceof LockableTileEntity)) {
 					ItemStack _setstack = new ItemStack(AluminumPlateItemItem.block, (int) (1));
+					_setstack.setCount((int) ((new Object() {
+						public int getAmount(BlockPos pos, int sltid) {
+							TileEntity inv = world.getTileEntity(pos);
+							if (inv instanceof LockableTileEntity) {
+								ItemStack stack = ((LockableTileEntity) inv).getStackInSlot(sltid);
+								if (stack != null)
+									return stack.getCount();
+							}
+							return 0;
+						}
+					}.getAmount(new BlockPos((int) x, (int) y, (int) z), (int) (2))) + 1));
+					((LockableTileEntity) inv).setInventorySlotContents((int) (2), _setstack);
+				}
+			}
+		} else if (((ItemTags.getCollection().getOrCreate(new ResourceLocation(("forge:gems/amethyst").toLowerCase(java.util.Locale.ENGLISH)))
+				.contains((new Object() {
+					public ItemStack getItemStack(BlockPos pos, int sltid) {
+						TileEntity inv = world.getTileEntity(pos);
+						if (inv instanceof LockableTileEntity)
+							return ((LockableTileEntity) inv).getStackInSlot(sltid);
+						return ItemStack.EMPTY;
+					}
+				}.getItemStack(new BlockPos((int) x, (int) y, (int) z), (int) (0))).getItem())) && (((new Object() {
+					public ItemStack getItemStack(BlockPos pos, int sltid) {
+						TileEntity inv = world.getTileEntity(pos);
+						if (inv instanceof LockableTileEntity)
+							return ((LockableTileEntity) inv).getStackInSlot(sltid);
+						return ItemStack.EMPTY;
+					}
+				}.getItemStack(new BlockPos((int) x, (int) y, (int) z), (int) (1))).getItem() == new ItemStack(PlateHammerItemItem.block, (int) (1))
+						.getItem()) && (((new Object() {
+							public int getAmount(BlockPos pos, int sltid) {
+								TileEntity inv = world.getTileEntity(pos);
+								if (inv instanceof LockableTileEntity) {
+									ItemStack stack = ((LockableTileEntity) inv).getStackInSlot(sltid);
+									if (stack != null)
+										return stack.getCount();
+								}
+								return 0;
+							}
+						}.getAmount(new BlockPos((int) x, (int) y, (int) z), (int) (2))) == 0) || ((new Object() {
+							public ItemStack getItemStack(BlockPos pos, int sltid) {
+								TileEntity inv = world.getTileEntity(pos);
+								if (inv instanceof LockableTileEntity)
+									return ((LockableTileEntity) inv).getStackInSlot(sltid);
+								return ItemStack.EMPTY;
+							}
+						}.getItemStack(new BlockPos((int) x, (int) y, (int) z), (int) (2)))
+								.getItem() == new ItemStack(AmethystPlateItemItem.block, (int) (1)).getItem()))))) {
+			{
+				TileEntity inv = world.getTileEntity(new BlockPos((int) x, (int) y, (int) z));
+				if (inv instanceof LockableTileEntity)
+					((LockableTileEntity) inv).decrStackSize((int) (0), (int) (1));
+			}
+			{
+				TileEntity inv = world.getTileEntity(new BlockPos((int) x, (int) y, (int) z));
+				if (inv != null && (inv instanceof LockableTileEntity)) {
+					ItemStack stack = ((LockableTileEntity) inv).getStackInSlot((int) (1));
+					if (stack != null) {
+						if (stack.attemptDamageItem((int) 1, new Random(), null)) {
+							stack.shrink(1);
+							stack.setDamage(0);
+						}
+					}
+				}
+			}
+			{
+				TileEntity inv = world.getTileEntity(new BlockPos((int) x, (int) y, (int) z));
+				if (inv != null && (inv instanceof LockableTileEntity)) {
+					ItemStack _setstack = new ItemStack(AmethystPlateItemItem.block, (int) (1));
+					_setstack.setCount((int) ((new Object() {
+						public int getAmount(BlockPos pos, int sltid) {
+							TileEntity inv = world.getTileEntity(pos);
+							if (inv instanceof LockableTileEntity) {
+								ItemStack stack = ((LockableTileEntity) inv).getStackInSlot(sltid);
+								if (stack != null)
+									return stack.getCount();
+							}
+							return 0;
+						}
+					}.getAmount(new BlockPos((int) x, (int) y, (int) z), (int) (2))) + 1));
+					((LockableTileEntity) inv).setInventorySlotContents((int) (2), _setstack);
+				}
+			}
+		} else if (((ItemTags.getCollection().getOrCreate(new ResourceLocation(("forge:ingots/chromium").toLowerCase(java.util.Locale.ENGLISH)))
+				.contains((new Object() {
+					public ItemStack getItemStack(BlockPos pos, int sltid) {
+						TileEntity inv = world.getTileEntity(pos);
+						if (inv instanceof LockableTileEntity)
+							return ((LockableTileEntity) inv).getStackInSlot(sltid);
+						return ItemStack.EMPTY;
+					}
+				}.getItemStack(new BlockPos((int) x, (int) y, (int) z), (int) (0))).getItem())) && (((new Object() {
+					public ItemStack getItemStack(BlockPos pos, int sltid) {
+						TileEntity inv = world.getTileEntity(pos);
+						if (inv instanceof LockableTileEntity)
+							return ((LockableTileEntity) inv).getStackInSlot(sltid);
+						return ItemStack.EMPTY;
+					}
+				}.getItemStack(new BlockPos((int) x, (int) y, (int) z), (int) (1))).getItem() == new ItemStack(PlateHammerItemItem.block, (int) (1))
+						.getItem()) && (((new Object() {
+							public int getAmount(BlockPos pos, int sltid) {
+								TileEntity inv = world.getTileEntity(pos);
+								if (inv instanceof LockableTileEntity) {
+									ItemStack stack = ((LockableTileEntity) inv).getStackInSlot(sltid);
+									if (stack != null)
+										return stack.getCount();
+								}
+								return 0;
+							}
+						}.getAmount(new BlockPos((int) x, (int) y, (int) z), (int) (2))) == 0) || ((new Object() {
+							public ItemStack getItemStack(BlockPos pos, int sltid) {
+								TileEntity inv = world.getTileEntity(pos);
+								if (inv instanceof LockableTileEntity)
+									return ((LockableTileEntity) inv).getStackInSlot(sltid);
+								return ItemStack.EMPTY;
+							}
+						}.getItemStack(new BlockPos((int) x, (int) y, (int) z), (int) (2)))
+								.getItem() == new ItemStack(ChromiumPlateItemItem.block, (int) (1)).getItem()))))) {
+			{
+				TileEntity inv = world.getTileEntity(new BlockPos((int) x, (int) y, (int) z));
+				if (inv instanceof LockableTileEntity)
+					((LockableTileEntity) inv).decrStackSize((int) (0), (int) (1));
+			}
+			{
+				TileEntity inv = world.getTileEntity(new BlockPos((int) x, (int) y, (int) z));
+				if (inv != null && (inv instanceof LockableTileEntity)) {
+					ItemStack stack = ((LockableTileEntity) inv).getStackInSlot((int) (1));
+					if (stack != null) {
+						if (stack.attemptDamageItem((int) 1, new Random(), null)) {
+							stack.shrink(1);
+							stack.setDamage(0);
+						}
+					}
+				}
+			}
+			{
+				TileEntity inv = world.getTileEntity(new BlockPos((int) x, (int) y, (int) z));
+				if (inv != null && (inv instanceof LockableTileEntity)) {
+					ItemStack _setstack = new ItemStack(ChromiumPlateItemItem.block, (int) (1));
 					_setstack.setCount((int) ((new Object() {
 						public int getAmount(BlockPos pos, int sltid) {
 							TileEntity inv = world.getTileEntity(pos);
